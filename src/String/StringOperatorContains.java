6dd52623e01653997a913d2e94707aab787e6c04
Admin <athiramohan7979@gmail.com>
2023-06-21 12:12:56 +0100

    first commit

---
+++ src/String/StringOperatorContains.java
@@ -0,0 +1,15 @@
+package String;
+
+
+public class StringOperatorContains
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 {
+		String x="welcome to Luminar";
+		System.out.println(x.contains("Luminar"));
+		
+	}
+
+}
+}
